47d37dad1f2f6b755cc777cad70d974cea1a2b7f
Onkar <101888473+Onkar8308@users.noreply.github.com>
2026-06-08 17:47:22 +0530

    added validators for logIn Form

--- src/app/feature/auth/pages/login/login.ts
+++ src/app/feature/auth/pages/login/login.ts
@@ -7,58 +7,53 @@ import {
   Validators
 } from '@angular/forms';
 
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 import { login } from '../../states/auth.actions';
 import { selectAuthError } from '../../states/auth.selectors';
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login implements OnInit {
-  loginForm: FormGroup;
-  errorMessage: string | null = null;
-
   private store = inject(Store);
   private destroyRef = inject(DestroyRef);
 
+  submitted = false;
+  loginForm: FormGroup;
+  errorMessage: string | null = 'Invalid Credentials';
+  error$ = this.store.select(selectAuthError);
+
   constructor(private fb: FormBuilder) {
+
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-zA-Z0-9._]{3,20}$/)
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/
+          )
+        ]
+      ]
     });
   }
 
   ngOnInit(): void {
-  }
-
-  clearError(): void {
-    this.errorMessage = null;
-  }
-
-  onSubmit(): void {
-    if (this.loginForm.invalid) return;
-
-    const { username, password } = this.loginForm.value;
-
-    this.store.dispatch(login({ username, password }));
-
-    this.store.select(selectAuthError)
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(error => {
-        if (!error) return;
-
-        this.errorMessage = error;
-      });
-
     this.loginForm.valueChanges
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(() => {
@@ -66,4 +61,25 @@ export class Login implements OnInit {
       });
   }
 
+  clearError(): void {
+    this.errorMessage = null;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) return;
+
+    const { username, password } = this.loginForm.value;
+
+    this.store.dispatch(login({ username, password }));
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
