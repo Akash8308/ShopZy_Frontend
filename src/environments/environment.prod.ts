c2ce2e7b1c0bb3c779198ba65223fbcfdd3f9dbe
Akash <86683095+Akash8308@users.noreply.github.com>
2026-06-11 01:02:09 +0530

    Environment file update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://shopzy-backend-a5ah.onrender.com',
+  apiBaseUrl: 'https://shopzy-backend-a5ah.onrender.com/api',
   apiTimeout: 30000,
 };
